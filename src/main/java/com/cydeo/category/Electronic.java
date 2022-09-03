e324a7cfa3d0f52fa1403fc2981fa93cf7194d38
Kadifa <Kudickadifa@gmail.com>
2022-09-02 20:21:06 -0500

    java-ecommerce-practice-Category

---
+++ src/main/java/com/cydeo/category/Electronic.java
@@ -0,0 +1,18 @@
+package com.cydeo.category;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Electronic extends Category{
+    public Electronic(UUID id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public LocalDateTime findDeliveryDueDate() {
+
+        LocalDateTime localDateTime=LocalDateTime.now();
+
+            return localDateTime.plusDays(4);
+    }
+}
