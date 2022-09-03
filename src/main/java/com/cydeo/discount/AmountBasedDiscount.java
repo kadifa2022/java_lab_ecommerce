4f363959bc4ccf4a3f645464dca21d6e86b08a91
Kadifa <Kudickadifa@gmail.com>
2022-09-03 07:35:22 -0500

    java-ecommerce-practice-Discount

--- src/main/java/com/cydeo/discount/AmountBasedDiscount.java
+++ src/main/java/com/cydeo/discount/AmountBasedDiscount.java
@@ -1,8 +1,23 @@
 package com.cydeo.discount;
 
-public class AmountBasedDiscount {
+import java.util.UUID;
+
+public class AmountBasedDiscount extends Discount{
 
     private Double discountAmount;
 
+    public AmountBasedDiscount(UUID id, String name, Double thresholdAmount) {
+        super(id, name, thresholdAmount);
+        this.discountAmount=discountAmount;
+    }
 
+
+    @Override
+    public Double calculateCartAmountAfterDiscountApplied(Double amount) {
+        return amount-discountAmount;
+    }
+
+    public Double getDiscountAmount() {
+        return discountAmount;
+    }
 }
