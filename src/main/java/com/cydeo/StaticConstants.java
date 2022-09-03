bf6724e34c823afdafef91436eeed229a4aeaa34
Kadifa <Kudickadifa@gmail.com>
2022-09-03 17:15:01 -0500

    java-ecommerce-practice-Static Constants

--- src/main/java/com/cydeo/StaticConstants.java
+++ src/main/java/com/cydeo/StaticConstants.java
@@ -2,6 +2,8 @@ package com.cydeo;
 
 import com.cydeo.balance.Balance;
 import com.cydeo.category.Category;
+import com.cydeo.discount.Discount;
+import com.cydeo.order.Order;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +15,8 @@ public class StaticConstants {
     public static final List<Product>PRODUCT_LIST=new ArrayList<>();
     public static final List<Balance>COSTUMER_BALANCE_LIST=new ArrayList<>();
     public static final List<Balance>GIFT_CARD_BALANCE_LIST=new ArrayList<>();
-    public static final List<Discount>DISCOUNT_LIST=new StaticConstants();
-    public static final List<Order>ORDER_LIST=new StaticConstants();
+    public static final List<Discount>DISCOUNT_LIST=new ArrayList<>();
+    public static final List<Order>ORDER_LIST=new ArrayList<>();
+
 
 }
